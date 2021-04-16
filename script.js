8996d0b05118d45b109688366237c8a52756f449
Lucas Nasci <36781651+lucasnsaraujo@users.noreply.github.com>
2021-04-16 17:46:42 -0300

    updated title

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-let modalOverlay = document.querySelector('.modal-overlay')
-const cards = document.querySelectorAll('.card')
-for(let card of cards) {
-    card.addEventListener('click', ()=> {
-        const iframe = document.querySelector('iframe');
-        const videoId = card.getAttribute("id")
-        iframe.src = `https://www.youtube.com/embed/${videoId}`
-        modalOverlay.classList.toggle('active')
-    })
+let modalOverlay = document.querySelector(".modal-overlay");
+const cards = document.querySelectorAll(".card");
+for (let card of cards) {
+  card.addEventListener("click", () => {
+    const iframe = document.querySelector("iframe");
+    const videoId = card.getAttribute("id");
+    iframe.src = `https://www.youtube.com/embed/${videoId}`;
+    modalOverlay.classList.toggle("active");
+  });
 }
 
-const closeModal = document.querySelector('.close-modal')
-closeModal.addEventListener('click', function() {
-    modalOverlay.classList.toggle('active')
-})
-
-
-iframe.src = ""
+const closeModal = document.querySelector(".close-modal");
+closeModal.addEventListener("click", function () {
+  modalOverlay.classList.toggle("active");
+});
 
 //https://www.youtube.com/embed/5-sfG8BV8wU
